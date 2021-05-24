9e7e0341c33166cb8615817eaf5bcd23e2670894
Juan José González <juanjose.gonzalez@morningstar.com>
2021-05-24 16:52:50 -0500

    creating fb enpoints

---
+++ src/server/middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+import { authJwt } from "../strategies/jwt.strategy";
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const { user } = req.session.passport;
+        if (user) {
+            req.user = user;
+            next();
+            return;
+        }
+        authJwt(req, res, next);
+    } catch {
+        authJwt(req, res, next);
+    }
+}
+
+export {
+    authMiddleware
+}
